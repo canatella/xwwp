c9863b8271afb8a93965800f655d72999ffcddf8
Damien Merenne <damien@bloom-life.com>
2020-03-22 09:11:41 +0100

    Update emacs dependency to 26.1 for `seq-sort-by` usage.

--- xwidget-plus.el
+++ xwidget-plus.el
@@ -5,7 +5,7 @@
 ;; Created: 2020-03-11
 ;; Keywords: convenience
 ;; Version: 0.1
-;; Package-Requires: ((emacs "25.3"))
+;; Package-Requires: ((emacs "26.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
