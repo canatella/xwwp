38e3b3099e57e66579c9799cbda3434a4e8f13b0
Damien Merenne <damien@bloom-life.com>
2020-03-31 10:00:32 +0200

    Follow link ido and helm depends on xwwp.

--- xwwp-follow-link-helm.el
+++ xwwp-follow-link-helm.el
@@ -5,7 +5,7 @@
 ;; Created: 2020-03-11
 ;; Keywords: convenience
 ;; Version: 0.1
-;; Package-Requires: ((emacs "26.1") (xwwp-follow-link "0.1"))
+;; Package-Requires: ((emacs "26.1") (xwwp "0.1"))
 
 ;; Copyright (C) 2020 Damien Merenne <dam@cosinux.org>
 
